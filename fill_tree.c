a9d67d82ce446e349e165794f7a3fa047c29c5d9
Artur HRYTSENKO <ahrytsen@e2r13p10.unit.ua>
2017-12-02 16:12:47 +0200

    fill

--- fill_tree.c
+++ fill_tree.c
@@ -6,7 +6,7 @@
 /*   By: ahrytsen <ahrytsen@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/01 20:43:43 by ahrytsen          #+#    #+#             */
-/*   Updated: 2017/12/02 15:31:01 by ahrytsen         ###   ########.fr       */
+/*   Updated: 2017/12/02 16:12:09 by ahrytsen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,16 @@ static t_tree	new_node(char k)
 	return (new_node);
 }
 
-static int	save_value(t_tree **head, char *keyword, char *value)
+static int		save_value(t_tree **head, char *keyword, char *value)
 {
 	t_tree *prev;
 	int		prev_direct;
 
 	prev
 	prev = NULL;
+	if (!*head)
+		if (!(*head = new_node(*keyword++)))
+				return (1);
 	while (*keyword)
 	{
 		if (!*head)
@@ -39,39 +42,29 @@ static int	save_value(t_tree **head, char *keyword, char *value)
 		}
 		else if ((*head)->letter == *keyword && *(keyword + 1))
 			head = &(*head)->down;
-		else
+		else if (*(keyword + 1))
 			head = &(*head)->right;
 		keyword++;
 	}
 	(*head)->value = value;
 }
 
-	fun()
-	{
-		while (str = get_ne)
-		{
-			ptr = head;
-			while (*ptr && *ptr->letter != *str)
-				ptr = &(*ptr)->right;
-			if (!*ptr)
-				*ptr = malloc(sizeof(t_tree *))
-
-		}
-	}
-t_tree	*fill_tree()
+t_tree			*fill_tree()
 {
 	t_tree	*head;
 	int		gnl_res;
 	char	*keyword;
 	char	*value;
 
-	while(!(gnl_res = get_next_line(0, &keyword)))
+	head = NULL;
+	while(!(gnl_res = get_next_line(0, &keyword)) && *keyword)
 	{
 		if (gnl_res == -1)
 			return (NULL);
 		gnl_res = get_next_line(0, &value);
 		if (gnl_res == -1 || !gnl_res)
 			return (NULL);
+		save_value(&head, keyword, value);
 	}
 	return (head);
 }
