5f6c3cbf6f8296123debca0cd96652bc5300f1c3
Andrii STADNIK <astadnik@student.unit.ua>
2017-12-02 15:26:17 +0200

    Changed prototypes in header

--- hotrace.h
+++ hotrace.h
@@ -6,7 +6,7 @@
 /*   By: astadnik <astadnik@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/01 20:18:22 by astadnik          #+#    #+#             */
-/*   Updated: 2017/12/02 14:59:33 by astadnik         ###   ########.fr       */
+/*   Updated: 2017/12/02 15:03:45 by astadnik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,18 +29,18 @@ struct	s_list
 **	Reads from stdin, fills t_table, returns NULL if Error occured
 */
 
-char	*fill_table(t_list	table[TABLE_SIZE]);
+char	*fill_table(t_list	*table[TABLE_SIZE]);
 
 /*
 **	Reads search queries, and prints value to standart output
 */
 
-void	print_results(t_list table[TABLE_SIZE]);
+void	print_results(t_list *table[TABLE_SIZE]);
 
 /*
 **	Frees every branch of the table
 */
 
-void	clean_table(t_table table[TABLE_SIZE]);
+void	clean_table(t_table *table[TABLE_SIZE]);
 
 #endif
